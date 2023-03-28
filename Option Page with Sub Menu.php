377f75a55fa1989a110ff0bf0ce7e7f1645380de
Muhammad Saud Farooque <37637301+MuhammadSaudFarooq@users.noreply.github.com>
2023-03-28 18:39:22 +0500

    Make option page with sub menu in wp admin side

---
+++ Option Page with Sub Menu.php
@@ -0,0 +1,32 @@
+<?php
+add_action('admin_menu', 'optionPageName_menu');
+function optionPageName_menu() {
+	add_menu_page(
+		'Option Page Title',
+		'Option Page Title',
+		'manage_options',
+		'optionPageName',
+		'optionPageName_page_callback',
+		'dashicons-edit',
+		50
+	);
+
+	add_submenu_page(
+		'optionPageName',
+		'Add New',
+		'Add New',
+		'manage_options',
+		'optionPageNewPostName',
+		'optionPageNewPostName_page_callback',
+	);
+}
+
+// Main option page file
+function optionPageName_page_callback() {
+	get_template_part('template-parts/file', 'name');
+}
+
+// Sub menu file name
+function optionPageNewPostName_page_callback() {
+	get_template_part('template-parts/file', 'name');
+}
